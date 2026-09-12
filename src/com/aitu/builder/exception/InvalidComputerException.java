d9f6baec86a82c49f30dc5607410a25a73a49b51
StudyAitu <muratbek.ibrai@gmail.com>
2026-09-12 08:25:20 +0500

    Delete src/com/aitu/builder directory

--- src/com/aitu/builder/exception/InvalidComputerException.java
+++
@@ -1,8 +0,0 @@
-package com.aitu.builder.exception;
-
-public class InvalidComputerException extends RuntimeException {
-
-    public InvalidComputerException(String message) {
-        super(message);
-    }
-}
